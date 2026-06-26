1e87ab6c4f05dcf4db8c051846f69e6a0ad3edec
Bruno Rafael <br781642@gmail.com>
2026-06-26 08:27:11 -0300

    Só o basico

---
+++ tarefa004/entregas/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Entregas
+
+admin.site.register(Entregas)
---
+++ tarefa004/entregas/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Entrega(models.Model):
+    nome = models.CharField(max_length=100)
+
+    STATUS_CHOICES = [
+        ('Preparando', 'Preparando'),
+        ('A caminho', 'A caminho'),
+        ('Entregue', 'Entregue'),
+    ]
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    prazo = models.DateField()
+
+    def __str__(self):
+        return self.nome
